ba616057c92b41254a8419cdac9bc7211d832ad6
TAHİR MAT <matahir33@gmail.com>
2022-07-08 13:11:13 +0300

    .

--- Code/Tutorial_1.py
+++ Code/Tutorial_1.py
@@ -1,4 +1,3 @@
-############ SELECT ALL TABLE ############
 
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -11,11 +10,21 @@ warnings.filterwarnings("ignore")
 # Your Database connection
 mydb = pymysql.connect(
     host='localhost', 
-    user='tahir', 
-    password='Password123#@!'
+    user='', 
+    password=''
 )
 mycursor = mydb.cursor()
 
+
+############ SELECT ALL TABLE ############ If you want to select all columns use this query and comment other queries
+query = '''
+    SELECT 
+        *
+    FROM
+        classicmodels.employees;
+'''
+
+############ SELECT ONE OR MORE COLUMNS ############ If you want to select specific column or columns use this query and comment other queries
 query = '''
     SELECT 
         employeeNumber, firstName, lastName
@@ -23,6 +32,7 @@ query = '''
         classicmodels.employees;
 '''
 
+# Execute the query
 mycursor.execute(query)
 result = mycursor.fetchall()
 
@@ -32,6 +42,5 @@ headers = [i[0] for i in mycursor.description]
 # Convert your query result ----> Dataframe.
 df = pd.DataFrame(result, columns=headers)
 
-print()
-print(df)
-print()
+# Print result
+print(f'\n\n{df}\n\n')
