2a18a4d1b3ac4b144752704c54a9f7e145613c36
TAHİR MAT <matahir33@gmail.com>
2022-07-08 11:39:50 +0300

    .

---
+++ Code/Tutorial_1.py
@@ -0,0 +1,37 @@
+############ SELECT ALL TABLE ############
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+import pandas as pd
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Your Database connection
+mydb = pymysql.connect(
+    host='localhost', 
+    user='tahir', 
+    password='Password123#@!'
+)
+mycursor = mydb.cursor()
+
+query = '''
+    SELECT 
+        employeeNumber, firstName, lastName
+    FROM
+        classicmodels.employees;
+'''
+
+mycursor.execute(query)
+result = mycursor.fetchall()
+
+# Get the column names from table
+headers = [i[0] for i in mycursor.description]
+
+# Convert your query result ----> Dataframe.
+df = pd.DataFrame(result, columns=headers)
+
+print()
+print(df)
+print()
