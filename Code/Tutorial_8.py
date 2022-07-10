d13d794e9a4cc9053aa78b31a0dbdfefc92c9483
TAHİR MAT <matahir33@gmail.com>
2022-07-10 17:16:13 +0300

    .

--- Code/Tutorial_8.py
+++ Code/Tutorial_8.py
@@ -15,9 +15,45 @@ mydb = pymysql.connect(
 )
 mycursor = mydb.cursor()
 
-############  ############ 
+############ ALIAS FOR COLUMNS ############ 
 query = '''
+	SELECT 
+		CONCAT_WS(', ', firstname, lastName) AS `Full name`
+	FROM
+		employees
+	ORDER BY `Full name`;
+'''
 
+############ ALIAS WITH GROUP BY AND HAVING ############ 
+query = '''
+    SELECT 
+        orderNumber `Order no.`,
+        SUM(priceEach * quantityOrdered) total
+    FROM
+        orderdetails
+    GROUP BY `Order no.`
+    HAVING total > 60000;
+'''
+
+############ ALIAS FOR TABLES ############ 
+query = '''
+	SELECT 
+		e.firstName, e.lastName
+	FROM
+		employees e
+	ORDER BY e.firstName; 
+'''
+
+############ WHEN BOTH TABLES HAVE THE SAME COLUMN NAME ############ 
+query = '''
+	SELECT 
+		customerName, COUNT(o.orderNumber) total
+	FROM
+		customers c
+			INNER JOIN
+		orders o ON c.customerNumber = o.customerNumber
+	GROUP BY customerName
+	ORDER BY total DESC;
 '''
 
 # Execute the query
